6f87d15ff59c650d06dfce3e7952ed76d070b3b2
narvdeshwar <sam.07soni@gmail.com>
2025-11-08 15:44:57 +0530

    docker build successfully till now

--- user-service/main.go
+++ user-service/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"user-services/handlers"
-
 	"github.com/gin-gonic/gin"
+	"log"
+	"user-services/db"
+	"user-services/handlers"
 )
 
 func main() {
+	database, err := db.ConnectDB()
+	if err != nil {
+		log.Fatal("Error in connecting to DB")
+	}
+	defer database.Close()
 	r := gin.Default()
-
 	r.POST("/user", handlers.CreateUser)
 	r.GET("/users", handlers.GetAllUser)
-
 	r.Run(":8081") // run on port 8081
 }
