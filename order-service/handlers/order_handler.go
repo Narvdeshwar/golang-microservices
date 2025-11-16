f3c16807991bdfeed3866e34cc0e0f36c90c9f7c
narvdeshwar <sam.07soni@gmail.com>
2025-11-16 15:00:23 +0530

    order creation and get all order api

--- order-service/handlers/order_handler.go
+++ order-service/handlers/order_handler.go
@@ -9,19 +9,19 @@ import (
 
 var orders = []models.Order{}
 
-func (h *Handlers) CreateOrder(ctx *gin.Context) {
+func (h *Handler) CreateOrder(ctx *gin.Context) {
 	var newOrder models.Order
 	if err := ctx.BindJSON(&newOrder); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
 
-	// Validating the user by calling the user services
+	// !TODO : user validation api required  
 	resp, err := http.Get("http://user-service:8081/users")
 	if err != nil || resp.StatusCode != 200 {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Error connecting to the user services"})
 	}
 
-	err = h.DB.QueryRow("Insert in orders (user_id,item,amount) values($1,$2,$3) Returning id", newOrder.UserID, newOrder.Item, newOrder.Amount).Scan(&newOrder.ID)
+	err = h.DB.QueryRow("Insert into orders (user_id,item,amount) values($1,$2,$3) Returning id", newOrder.UserID, newOrder.Item, newOrder.Amount).Scan(&newOrder.ID)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the Order"})
 		return
@@ -30,6 +30,25 @@ func (h *Handlers) CreateOrder(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "Order Created Successfully", "data": newOrder})
 }
 
-func GetAllOrder(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, orders)
+func (h *Handler) GetAllOrder(ctx *gin.Context) {
+	rows, err := h.DB.Query("Select id,user_id,item,amount from orders")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch orders"})
+		return
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var o models.Order
+		if err := rows.Scan(&o.ID, &o.UserID, &o.Item, &o.Amount); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error scanning the user data"})
+			return
+		}
+		orders = append(orders, o)
+	}
+	if len(orders) == 0 {
+		ctx.JSON(http.StatusOK, gin.H{"message": "No Order found"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": orders})
 }
--- order-service/main.go
+++ order-service/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"order-services/handlers"
+	"log"
+	"order-services/db"
+	handlers "order-services/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	database, err := db.ConnectDB()
+	if err != nil {
+		log.Fatal("Error connecting to order DB", err)
+	}
+	defer database.Close()
+
+	h := &handlers.Handler{DB: database}
 	r := gin.Default()
 
-	r.POST("/orders", handlers.CreateOrder)
+	r.POST("/orders", h.CreateOrder)
 
-	r.GET("/orders", handlers.GetAllOrder)
+	r.GET("/orders", h.GetAllOrder)
 
 	r.Run(":8082")
 
