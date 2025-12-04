4139c40d1ef3f90ffc8ddfdbe7604838d8526868
Narvdeshwar <sam.07soni@gmail.com>
2025-12-04 15:07:13 +0530

    added main.go for auth-services

--- auth-service/main.go
+++ auth-service/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "log"
+import (
+	"auth-service/db"
+	"auth-service/handlers"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	log.Printf("hi")
+	database, err := db.ConnectDB()
+	if err != nil {
+		log.Println("Error connecting to database")
+		return
+	}
+	defer database.Close()
+
+	h := &handlers.Handler{DB: database}
+	r := gin.Default()
+
+	r.POST("/register", h.RegisterUser)
+	r.POST("/login", h.LoginUser)
+
+	r.Run(":8085")
+
 }
