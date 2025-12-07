98d1eb23a0908912c692d5d8093f37f90f14ce60
narvdeshwar <sam.07soni@gmail.com>
2025-12-07 13:20:23 +0530

    added request proxy

--- gateway-service/routes/proxy.go
+++ gateway-service/routes/proxy.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ReverseProxy(target string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		url, _ := url.Parse(target)
+		proxy := httputil.NewSingleHostReverseProxy(url)
+		ctx.Request.URL.Host = url.Host
+		ctx.Request.URL.Scheme = url.Scheme
+		ctx.Request.Host = url.Host
+		proxy.ServeHTTP(ctx.Writer, ctx.Request)
+	}
+}
