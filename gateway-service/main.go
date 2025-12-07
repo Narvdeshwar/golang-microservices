7957ba3da09beea10867f359a801460f991d9657
narvdeshwar <sam.07soni@gmail.com>
2025-12-07 13:29:10 +0530

    added main route handler

--- gateway-service/main.go
+++ gateway-service/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"gateway-service/middleware"
+	"gateway-service/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(middleware.AuthMiddleware())
+
+	r.Any("/api/auth/*any", routes.ReverseProxy("http://auth-service:8085"))
+	r.Any("/api/user/*any", routes.ReverseProxy("http://user-service:8081"))
+	r.Any("/api/order/*any", routes.ReverseProxy("http://order-service:8082"))
+	r.Any("/api/payment/*any", routes.ReverseProxy("https://payment-service:8083"))
+	r.Run(":8080")
+}
